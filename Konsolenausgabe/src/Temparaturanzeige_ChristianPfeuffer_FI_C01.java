4d109adf17cbe012a8b75feab075afda7aa424d0
ChrisPfeuffer <63845180+ChrisPfeuffer@users.noreply.github.com>
2020-09-11 09:14:29 +0200

    %f instead of Math.round usage

--- Konsolenausgabe/src/Temparaturanzeige_ChristianPfeuffer_FI_C01.java
+++ Konsolenausgabe/src/Temparaturanzeige_ChristianPfeuffer_FI_C01.java
@@ -5,12 +5,12 @@ public class Temparaturanzeige_ChristianPfeuffer_FI_C01 {
 		System.out.println("--------------------------");
 		for (int i = -20; i < 0; i+=10) {
 			double  celsius =(( 5 *(i - 32.0)) / 9.0);
-			  System.out.printf("%-12d | %10s\n", i, Math.round(celsius * 100) / 100.0);
+			  System.out.printf("%-12d | %10.2f\n", i, celsius);
 			}
-		System.out.printf("%-12s | %10s\n","+0", Math.round((( 5 *(0 - 32.0)) / 9.0) * 100) / 100.0);
+		System.out.printf("%-12s | %10.2f\n","+0", (( 5 *(0 - 32.0)) / 9.0));
 		for (int i = 20; i < 40; i+=10) {
 			double  celsius =(( 5 *(i - 32.0)) / 9.0);
-			  System.out.printf("+%-11d | %10s\n", i, Math.round(celsius * 100) / 100.0);
+			  System.out.printf("+%-11d | %10.2f\n", i, celsius);
 			}
 	}
 }
