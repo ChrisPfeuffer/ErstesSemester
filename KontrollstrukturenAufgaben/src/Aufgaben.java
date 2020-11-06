979d7b00cb7007b00d1e6b82b89fe3954d94346b
Christian Pfeuffer <chris_pfeuffer@web.de>
2020-11-06 10:49:00 +0100

    finished Aufgabe1 von Kontrollstrukturen

---
+++ KontrollstrukturenAufgaben/src/Aufgaben.java
@@ -0,0 +1,22 @@
+
+public class Aufgaben {
+
+	public static void main(String[] args) {
+		int note = 4;
+		
+		switch(note) {
+		case 1: System.out.println("Sehr Gut");
+		break;
+		case 2: System.out.println("Gut");
+		break;
+		case 3: System.out.println("Befriedigend");
+		break;
+		case 4: System.out.println("Ausreichend");
+		break;
+		case 5: System.out.println("Mangelhaft");
+		break;
+		case 6: System.out.println("Ungenügend");
+		break;
+		}
+	}
+}
