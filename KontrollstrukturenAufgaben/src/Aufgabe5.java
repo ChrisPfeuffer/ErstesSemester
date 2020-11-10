1ad987497cdbbb31b33098eeca31c4f979fc4320
Christian Pfeuffer <chris_pfeuffer@web.de>
2020-11-10 15:05:01 +0100

    Ich hab festgelegt, dass Mwst in bestellwerteingabe schon sind

--- KontrollstrukturenAufgaben/src/Aufgabe5.java
+++ KontrollstrukturenAufgaben/src/Aufgabe5.java
@@ -11,15 +11,15 @@ public class Aufgabe5 {
 	
 	public static void rabattsystem(double bestellcost) {
 		if (bestellcost >= 0 && bestellcost <= 100) {
-			double mitRabatt = bestellcost * 0.9 * 1.16;
+			double mitRabatt = bestellcost * 0.9;
 			System.out.printf("Mit einem Rabatt von 10 Prozent liegt der Bestellwert nun bei: %.2f€ incl. Mwst", mitRabatt);
 		}
 		else if (bestellcost > 100 && bestellcost <= 500) {
-			double mitRabatt = bestellcost * 0.85 * 1.16;
+			double mitRabatt = bestellcost * 0.85;
 			System.out.printf("Mit einem Rabatt von 15 Prozent liegt der Bestellwert nun bei: %.2f€ incl. Mwst", mitRabatt);
 		}
 		else {
-			double mitRabatt = bestellcost * 0.80 * 1.16;
+			double mitRabatt = bestellcost * 0.80;
 			System.out.printf("Mit einem Rabatt von 20 Prozent liegt der Bestellwert nun bei: %.2f€ incl. Mwst", mitRabatt);
 		}
 	}
