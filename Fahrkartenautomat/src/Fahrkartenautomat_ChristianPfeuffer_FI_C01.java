90b9cf33ed3924c574c05d48bac39fc83321a07d
Christian Pfeuffer <chris_pfeuffer@web.de>
2020-11-18 16:05:08 +0100

    Aufgabe 06 fertig

--- Fahrkartenautomat/src/Fahrkartenautomat_ChristianPfeuffer_FI_C01.java
+++ Fahrkartenautomat/src/Fahrkartenautomat_ChristianPfeuffer_FI_C01.java
@@ -4,10 +4,29 @@ class Fahrkartenautomat_ChristianPfeuffer_FI_C01
 {
 	public static double fahrkartenbestellungErfassen(){
 		Scanner tastatur = new Scanner(System.in);
-		System.out.print("Zu zahlender Betrag (EURO): ");
-	    double p1 = tastatur.nextDouble();
-       
-	    System.out.print("Wieviel Fahrkarten wollen Sie? ");
+		double p1 = 0.0;
+		
+		while (p1 == 0.0) {
+			System.out.println("Wählen Sie ihre Wunschfahrkarte für Berlin AB aus:\r\n"
+					+ "  Einzelfahrschein Regeltarif AB [2,90 EUR] (1)\r\n"
+					+ "  Tageskarte Regeltarif AB [8,60 EUR] (2)\r\n"
+					+ "  Kleingruppen-Tageskarte Regeltarif AB [23,50 EUR] (3)\n \n");
+			System.out.print("Ihre Wahl: ");
+		    int eingabeFahrkarten = tastatur.nextInt();
+		    switch (eingabeFahrkarten) {
+		    case 1: p1 = 2.90;
+		    break;
+		    case 2: p1 = 8.60;
+		    break;
+		    case 3: p1 = 23.50;
+		    break;
+		    default: System.out.println("Error. Mit Ihrer Eingabe kann ich nichts anfangen.\n \n"
+		    		+ "----------------------------------------------");
+		    		p1 = 0.0;	
+		    }
+		}
+	    
+	    System.out.print("Anzahl Tickets: ");
 	    byte p2 = tastatur.nextByte();
 
 	    while (p2 > 10 || p2 <= 0) {
@@ -123,11 +142,11 @@ class Fahrkartenautomat_ChristianPfeuffer_FI_C01
 		System.out.println("\n\n");
 	}
 	
-	
-	
     public static void main(String[] args)
     {
     	while(true) {
+    		System.out.println("Fahrkartenbestellvorgang: \n"
+    				+ "========================= \n");
 	    	// Bestellung
 	    	double zuzahlendenBetrag = fahrkartenbestellungErfassen();
 	       
